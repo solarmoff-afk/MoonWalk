b436640503c1aaed1ea4974d870cc1f48355219e
UpdateDeveloper <solarmoff@gmail.com>
2026-01-02 18:12:08 +0300

    wip: Рефакторинг api, теперь все публичные api методы кроме new вынесены в public

---
+++ moonwalk/src/public/mod.rs
@@ -0,0 +1,17 @@
+// Часть проекта MoonWalk с открытым исходным кодом.
+// Лицензия EPL 2.0, подробнее в файле LICENSE. Copyright (c) 2025 MoonWalk
+
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+mod export;
+mod filters;
+mod objects;
+mod resources;
+
+pub use export::*;
+pub use filters::*;
+pub use objects::*;
+pub use resources::*;
+
+pub use crate::{MoonWalk, TextAlign, FontAsset};
