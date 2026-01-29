7b6463f5d874339291971c434938499e2ad40e94
UpdateDeveloper <solarmoff@gmail.com>
2026-01-29 23:14:17 +0300

    refactor(backend): Сделал рефакторинг texture.rs убрав дубляж кода и добавил create_depth_texture

--- backend/src/render/texture.rs
+++ backend/src/render/texture.rs
@@ -14,6 +14,13 @@ pub enum BackendTextureFormat {
     Bgra8UnormSrgb = 2,
 }
 
+// Приватное перечисление для определения типа текстуры
+enum TextureType {
+    Default = 1,
+    RenderTarget = 2,
+    Depth = 3,
+}
+
 pub struct BackendTextureConfig {
     format: BackendTextureFormat,
     label: String,
@@ -111,13 +118,50 @@ impl BackendTexture {
         width: u32,
         height: u32,
     ) -> Result<(), MoonBackendError> {
+        self.create_texture(context, Some(bytes), width, height, TextureType::Default)
+    }
+
+    pub fn create_render_target(
+        &mut self,
+        context: &mut BackendContext,
+        width: u32,
+        height: u32,
+    ) -> Result<(), MoonBackendError> {
+        self.create_texture(context, None, width, height, TextureType::RenderTarget)
+    }
+
+    pub fn create_depth_texture(
+        &mut self,
+        context: &mut BackendContext,
+        width: u32,
+        height: u32,
+    ) -> Result<(), MoonBackendError> {
+        self.create_texture(context, None, width, height, TextureType::Depth)
+    }
+
+    fn create_texture(
+        &mut self,
+        context: &mut BackendContext,
+        bytes: Option<&[u8]>,
+        width: u32,
+        height: u32,
+        texture_type: TextureType,
+    ) -> Result<(), MoonBackendError> {
         match &mut context.get_raw() {
             Some(raw_context) => {
+                // Общие блоки кода для всех
+
                 // Размер текстуры
                 let size = self.pack_size(width, height);
 
                 let format = self.config.get_format();
                 let texture_format = self.map_format_to_wgpu(format);
+
+                let usage = match texture_type {
+                    TextureType::Default => self.get_usage(),
+                    TextureType::RenderTarget => self.get_usage(),
+                    TextureType::Depth => self.get_usage_depth(),
+                };
 
                 let texture = raw_context.device.create_texture(&wgpu::TextureDescriptor {
                     // То самое название из конфига
@@ -135,35 +179,55 @@ impl BackendTexture {
                     // Формат
                     format: texture_format,
                     
-                    usage: self.get_usage(),
+                    usage,
                     view_formats: &[],
                 });
 
-                // Запись в текстуру через очередь в сыром контексте
-                raw_context.queue.write_texture(
-                    wgpu::TexelCopyTextureInfo {
-                        texture: &texture,
-                        mip_level: 0,
-                        origin: wgpu::Origin3d::ZERO,
-                        aspect: wgpu::TextureAspect::All,
-                    },
+                // Запись в текстуру через очередь в сыром контексте только если
+                // texture_type это дефолтная текстура, так как иначе запись
+                // не нужна
 
-                    bytes,
+                if let TextureType::Default = texture_type {
+                    raw_context.queue.write_texture(
+                        wgpu::TexelCopyTextureInfo {
+                            texture: &texture,
+                            mip_level: 0,
+                            origin: wgpu::Origin3d::ZERO,
+                            aspect: wgpu::TextureAspect::All,
+                        },
 
-                    wgpu::TexelCopyBufferLayout {
-                        offset: 0,
-                        bytes_per_row: Some(4 * width),
-                        rows_per_image: Some(height),
-                    },
+                        // Я уверен в том что паники здесь не будет, так как
+                        // этот блок выполняется ТОЛЬКО если texture_type это
+                        // TextureType::Default (обычная текстура), а такое
+                        // возможно только если вызов был из функции from_raw,
+                        // а там массив байтов обязательный, без Option
+                        // Паника тут может быть только в случае затупа, поэтому
+                        // нужно проверять это всё. Без .unwrap() код был бы
+                        // длиннее, но прикол в том что один этот комментарий
+                        // длиннее чем код match который нужен для проверки на
+                        // None, лол ;)
+                        bytes.unwrap(),
 
-                    size,
-                );
+                        wgpu::TexelCopyBufferLayout {
+                            offset: 0,
+                            bytes_per_row: Some(4 * width),
+                            rows_per_image: Some(height),
+                        },
 
+                        size,
+                    );
+                }
+
+                // Снова общие для всех части
                 let view = texture.create_view(
                     &wgpu::TextureViewDescriptor::default()
                 );
                 
-                let sampler_descriptor = self.get_sampler_descriptor();
+                let sampler_descriptor = match texture_type {
+                    TextureType::Default | TextureType::RenderTarget => self.get_sampler_descriptor(),
+                    TextureType::Depth => self.get_sampler_descriptor_depth(),
+                };
+
                 let sampler = raw_context.device.create_sampler(&sampler_descriptor);
                 
                 let bind_group_layout = self.create_bind_group_layout(&raw_context);
@@ -186,60 +250,20 @@ impl BackendTexture {
         }
     }
 
-    /// Метод для создания рендер таргета
-    pub fn create_render_target(
-        &mut self,
-        context: &mut BackendContext,
-        width: u32,
-        height: u32,
-    ) -> Result<(), MoonBackendError> {
-        match &mut context.get_raw() {
-            Some(raw_context) => {
-                let size = self.pack_size(width, height);
-
-                let format = self.config.get_format();
-                let texture_format = self.map_format_to_wgpu(format);
-                
-                let texture = raw_context.device.create_texture(
-                    &wgpu::TextureDescriptor {
-                        label: Some(&self.config.get_label()),
-                        size,
-                        mip_level_count: 1,
-                        sample_count: 1,
-                        dimension: wgpu::TextureDimension::D2,
-                        format: texture_format,
-                        usage: self.get_usage(),
-                        view_formats: &[],
-                    }
-                );
-
-                let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-                
-                let sampler_descriptor = self.get_sampler_descriptor();
-                let sampler = raw_context.device.create_sampler(&sampler_descriptor);
-                
-                let bind_group_layout = self.create_bind_group_layout(&raw_context);
-                let bind_group = self.create_bind_group(
-                    &raw_context, &view, &bind_group_layout, &sampler,
-                );
-
-                self.width = width;
-                self.height = height;
-                self.raw = Some(RawTexture::new(texture, view, sampler, bind_group));
-
-                Ok(())
-            }
-
-            None => Err(MoonBackendError::ContextNotFoundError)
-        }
-    }
-
     /// Хард код usage в метод, так как в константу нельзя :(
     fn get_usage(&self) -> wgpu::TextureUsages {
         wgpu::TextureUsages::TEXTURE_BINDING 
             | wgpu::TextureUsages::RENDER_ATTACHMENT 
             | wgpu::TextureUsages::COPY_SRC
             | wgpu::TextureUsages::COPY_DST
+    }
+
+    /// Специальный хардкод для depth. Отсуствуют COPY_SRC и COPY_DST
+    /// так как они не нужны depth текстуре. Я не знаю влияет ли положительно
+    /// отсуствие двух usage, но на всякий случай было бы неплохо убрать
+    fn get_usage_depth(&self) -> wgpu::TextureUsages {
+        wgpu::TextureUsages::RENDER_ATTACHMENT 
+            | wgpu::TextureUsages::TEXTURE_BINDING
     }
 
     /// Хардкод сэмлер дескриптора через метод
@@ -255,6 +279,19 @@ impl BackendTexture {
             mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        }
+    }
+
+    /// Хардкод сэмлер дескриптора через метод специально для текстуры
+    /// глубины (depth)
+    fn get_sampler_descriptor_depth(&self) -> wgpu::SamplerDescriptor<'_> {
+        // [MAYBE]
+        // [HARDCODE]
+        wgpu::SamplerDescriptor {
+            compare: Some(wgpu::CompareFunction::LessEqual),
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
             ..Default::default()
         }
     }
